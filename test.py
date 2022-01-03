51f499d54c7c3869b1bb0adfa4e11dff5d9b815a
Arda Sak <ardasak434@gmail.com>
2022-01-04 00:41:51 +0300

    Making compatible for pytest

--- test.py
+++
@@ -1,3 +0,0 @@
-import air_quality_lib
-
-print(air_quality_lib.City("New York City").get_city())
---
+++ test_app.py
@@ -0,0 +1,4 @@
+import air_quality_lib
+
+def test_app():
+    print(air_quality_lib.City("New York City").get_city())
